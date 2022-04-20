ca36c4dbad4f062be3b6c419d776e5759a91feba
chinmaynehate <chinmaynehate@gmail.com>
2022-04-21 02:51:14 +0530

    sort files by name

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -137,13 +137,14 @@ def get_video_paths(data_dir, num_videos):
     ext = ['mp4', 'avi', 'mkv','wmv']    # Add image formats here
     files = []
     [files.extend(glob(data_dir + '*.' + e,recursive=True)) for e in ext]
+    files.sort(reverse=False)
     #video_paths = glob(data_dir + "**/*.mp4", recursive=True)
     video_paths = files
     # video_idxs = [2,5,6,7]# [x for x in range(0,num_videos)]  # if num_videos is 3, then video_idxs = [0,1,2] i.e we will test videos at index 0,1,2 in file_names
     file_names = []
     for i in video_paths:
         file_names.append(os.path.basename(i))
-    file_names.sort()
+    file_names.sort(reverse=False)
     #print("videos:",files)
     return file_names, video_paths
 
